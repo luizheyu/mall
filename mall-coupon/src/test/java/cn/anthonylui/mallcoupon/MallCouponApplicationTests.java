cad4d6ff13195a957ecfbcf8276fdc394c27c155
AnthonyLui <646964351@qq.com>
2020-12-11 15:24:25 +0800

    structure

---
+++ mall-coupon/src/test/java/cn/anthonylui/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.anthonylui.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mall-ware/src/test/java/cn/anthonylui/mallware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.anthonylui.mallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
